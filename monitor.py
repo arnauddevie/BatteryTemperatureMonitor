22e43c979670b5732cf39139369495c5929e5e7e
Arnaud Devie <arnaud.devie@hawaii.edu>
2016-09-15 11:53:42 -1000

    Initial commit

---
+++ monitor.py
@@ -0,0 +1,102 @@
+import os
+import re
+import csv
+import glob
+import time
+from temperature_sensor_class import *
+
+def deblank(string:str):
+    return string.lstrip().rstrip()
+
+celsius= u'\N{DEGREE SIGN}' + 'C'
+
+#sensor_base_address = '28-000007'
+sensor_base_address = ''
+sensorID_pattern = '[0-9A-Fa-f]{6}'
+regexp = re.compile(sensorID_pattern)
+
+sensor_list = list()
+program_list = list()
+cellID_list = list()
+temperature_list = list()
+
+with open('mapping.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        if len(row) is 4:
+            if regexp.search(row[0]) is not None:
+                sensor_list.append(sensor_base_address + deblank(row[0]))
+                program_list.append(deblank(row[1]))
+                cellID_list.append(deblank(row[2]))
+                temperature_list.append(int(row[3]))
+
+print('Found {} valid rows in mapping.CSV'.format(len(sensor_list)))
+
+program_dict = dict(zip(sensor_list, program_list))
+cellID_dict = dict(zip(sensor_list, cellID_list))
+temperature_dict = dict(zip(sensor_list, temperature_list))
+
+# Turn ON GPIO and temperature sensors
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+registered_devices = set()
+cells = list()
+
+while True:
+    device_folder = glob.glob(base_dir + '28*')
+
+    attached_devices = set()
+    detached_devices = set(sensor_list)
+    lost_devices = set()
+
+
+    for device in device_folder:
+        sensor_ID = device[-6:]
+        if sensor_ID not in attached_devices:
+            attached_devices.add(sensor_ID)
+            detached_devices.discard(sensor_ID)
+            print('Sensor ID {} attached @ {}'.format(sensor_ID, time.ctime()))
+
+    lost_devices = registered_devices.difference(attached_devices)
+    for sensor_ID in lost_devices:
+        registered_devices.discard(sensor_ID)
+        cells[:] = [cell for cell in cells if cell.sensorID is not sensor_ID]
+        print('Sensor ID {} lost @ {}'.format(sensor_ID, time.ctime()))
+    	
+    for sensor_ID in attached_devices:
+        if sensor_ID not in registered_devices:
+            registered_devices.add(sensor_ID)
+            print('Sensor ID {} registered @ {}'.format(sensor_ID, time.ctime()))
+            cells.append(MonitoredCell(cellID_dict[sensor_ID], base_dir, sensor_ID, program_dict[sensor_ID], 'NCRB', '001', temperature_dict[sensor_ID], 27, 29, False))
+
+    print(attached_devices)
+    print('::::::::::::::')
+    print(detached_devices)
+    print('::::::::::::::')
+    print(registered_devices)
+    print('::::::::::::::')
+    print(lost_devices)
+
+    print('\nDetected the following sensors:\n')
+    for cell in cells:
+        try:
+            cell.update_Temperature()
+            cell.check_Status()
+            print('Sensor ID: '+ cell.sensorID)
+            print('Cell ID: ' + cell.cellID)
+            print('Ambient temperature: ' + str(cell.ambientTemperature) + celsius)
+            print('Warning: ' + str(cell.warningThreshold) + celsius)
+            print('Alarm: ' + str(cell.alarmThreshold) + celsius)
+            print('These settings are valid') if cell.validSettings else print('These settings are incorrect')
+            print('Current temperature: ' + str(cell.currentTemperature) + celsius)
+            print('Average temperature: ' + str(cell.averageTemperature) + celsius)
+            print('Status: ' + cell.status)
+            print('\n')
+        except NameError:
+            print('Could not read from device ' + cell.sensorID + '\n')
+        except FileNotFoundError:
+            print('Could not locate file for device ' + cell.sensorID + '\n')
+    time.sleep(10)
+
---
+++ temperature_sensor_class.py
@@ -0,0 +1,135 @@
+import typecheck as tc
+
+class MonitoredCell(object):
+    """A battery cell with a DS18B20 temperature sensor for monitoring.
+
+    Attributes:
+        cellID: The full nickname of the cell in the Pakali system, as a string.
+        sensorPath: The full path pointing to the folder where sensors' data are stored, as a string.
+        sensorID: The 14-digit unique ID of the temperature sensor, as a string.
+        make: The make of the cell, as a string.
+        model: The model of the cell, as a string.
+        number: The number of the cell in the Pakali system, as a string.
+        ambientTemperature: The ambient temperature of the climate chamber, as an integer.
+        warningThreshold: The warning level temperature, as an integer.
+        alarmThreshold: The alarm level temperature, as an integer.
+        enableLogging: Enable or disable logging of measured values to logfile, as a boolean.
+        currentTemperature: The current temperature reported by the sensor, as a float.
+        averageTemperature: The moving average of the measured temperature over the past (10) readings, as a float.
+        recentReadings: The past (10) readings, as a list of floats.
+        absoluteMaximumTemperature: The critical level temperature for all instances (non-editable)
+        validSettings: editable settings will be checked at initialization to make sure they are valid (non-editable)
+        status: current status('Normal'|'Warning'|'Alarm'), as a string.
+    """
+    @tc.typecheck
+    def __init__(self, cellID:str, sensorPath:str, sensorID:str, make:str, model:str, number:str, ambientTemperature:int, warningThreshold:int, alarmThreshold:int, enableLogging:bool):
+        """Return a new MonitoredCell object."""
+
+        # Static setting common to all instances of this class (high level fail-safe)
+        absoluteMaximumTemperature = 80
+
+        # Editable
+        self.cellID = cellID
+        self.sensorPath = sensorPath
+        self.sensorID = '28-000007' + sensorID
+        self.make = make
+        self.model = model
+        self.number = number
+        self.ambientTemperature = ambientTemperature
+        self.warningThreshold = warningThreshold
+        self.alarmThreshold = alarmThreshold
+        self.enableLogging = enableLogging
+
+        # Read only
+        self.currentTemperature = 0.0
+        self.averageTemperature = 0.0
+        self.recentReadings = [0.0]*10
+        self.absoluteMaximumTemperature = absoluteMaximumTemperature
+
+        # Checking values of Attributes (typechecking is done by typecheck-decorator module at runtime)
+        # Empty strings are "False"
+        # Integer must be in correct range
+        if  (
+            cellID is not '' and
+            make is not '' and
+            model is not '' and
+            number is not '' and
+            ambientTemperature in range (-100, absoluteMaximumTemperature) and
+            warningThreshold in range(ambientTemperature, alarmThreshold) and
+            alarmThreshold in range(ambientTemperature, absoluteMaximumTemperature)
+            ):
+            self.validSettings = True
+        else:
+            self.validSettings = False
+
+    # Placeholder methods
+
+    def read_temp_raw(self,device_file):
+        with open(device_file, 'r') as f:
+            lines = f.readlines()
+        return lines
+
+    def read_temp(self):
+        import os
+        import time
+        lines = self.read_temp_raw(os.path.join(self.sensorPath, self.sensorID, 'w1_slave'))
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            # temp_f = temp_c * 9.0 / 5.0 + 32.0
+            # return temp_f
+            return temp_c
+
+    def sendSMS(self):
+        from twilio.rest import TwilioRestClient
+
+        # Your Account Sid and Auth Token from twilio.com/user/account
+        account_sid = "AC3d1d94b03a9c324312b82ed931cde90e"
+        auth_token  = "1b4c8b535453b43a35ea686a7aa60576"
+        client = TwilioRestClient(account_sid, auth_token)
+
+        message = client.messages.create(body = self.cellID + self.sensorID + self.status,
+            to="+18083877670",    # Replace with your phone number
+            from_="+18082010840") # Replace with your Twilio number
+        print(message.sid)
+
+
+    def get_warningThreshold(self):
+        """Return the warning level temperature as an integer."""
+        return self.warningThreshold
+
+    def get_alarmThreshold(self):
+        """Return the alarm level temperature as an integer."""
+        return self.alarmThreshold
+
+    def update_Temperature(self):
+        """Update the current temperature in Celsius as an float."""
+        # Read current value from sensor
+        self.currentTemperature = self.read_temp()
+        # Drop the oldest data point
+        self.recentReadings.pop()
+        # Insert the new data point at the beginning of the list
+        self.recentReadings.insert(0, self.currentTemperature)
+        # Compute a new average temperature
+        self.averageTemperature = sum(reading for reading in self.recentReadings) / len(self.recentReadings)
+
+        return self.currentTemperature, self.averageTemperature
+
+    def check_Status(self):
+        """Checks and reports the current temperature against the warning and alarm level temperature, as a string."""
+
+        if self.currentTemperature > self.alarmThreshold:
+            self.status = 'Alarm'
+        elif self.currentTemperature > self.warningThreshold:
+            self.status = 'Warning'
+        else:
+            self.status = 'Normal'
+
+        #self.sendSMS()
+
+        return self.status
+
